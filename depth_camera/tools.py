84f53efa2a220c969d11823fff8e9d592ed291ac
Tamtemtom020604 <timo.widyanvolta-2021@ftmm.unair.ac.id>
2024-01-30 10:34:00 +0700

    Make DepthStream and ColorStream Class

--- depth_camera/tools.py
+++ depth_camera/tools.py
@@ -29,19 +29,11 @@ class DepthCamera :
             width=640,
             height=480,
             fps=30,
-            save_data = True,
+            save_data = False,
             ):
         
         ##---------------------------------------------------------------------------------------------------------
         # PARAMETER CHECKING
-        self.height = height
-        self.depth = depth
-        
-        if depth == False:
-            img_depth = None
-            depth_image = None
-        if color == False:
-            color_image = None
         
         for args in [depth, color, save_data]:
             if isinstance(args, bool) == False:
@@ -66,16 +58,17 @@ class DepthCamera :
         ##---------------------------------------------------------------------------------------------------------
         # DEPTH STREAM INITIALITATION
         if depth:
-            openni2.initialize(self.redist)
-            dev = openni2.Device.open_any()
-            depth_stream = dev.create_depth_stream()
-            depth_stream.start()
-            depth_stream.set_video_mode(c_api.OniVideoMode(pixelFormat = c_api.OniPixelFormat.ONI_PIXEL_FORMAT_DEPTH_1_MM, resolutionX = width, resolutionY = height, fps = fps))
+            depth_stream = DepthStream(redist=self.redist, width=width, height=height, fps=fps)
+        else :
+            img_depth = None
+            depth_image = None
         
         ##---------------------------------------------------------------------------------------------------------
         # COLOR STREAM INITIALITATION
         if color:
-            cap = cv2.VideoCapture(self.cam)
+            color_stream = ColorStream(cam=self.cam, model=self.model)
+        else : 
+            color_image = None
             
         if temporal_filter:
             prev_color_image=None
@@ -88,13 +81,7 @@ class DepthCamera :
             ##----------------------------------------------------------------------------------------------------
             # GET DEPTH FRAME 
             if depth:
-                depth_frame = depth_stream.read_frame()
-                depth_frame_data = depth_frame.get_buffer_as_uint16()
-                img_depth = np.frombuffer(depth_frame_data, dtype=np.uint16).astype(np.float32)
-                img_depth.shape = (480, 640)
-                depth_image = cv2.normalize(img_depth, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-                
-                # depth_image = cv2.applyColorMap(depth_image, cv2.COLORMAP_JET)
+                depth_image, img_depth = depth_stream.get_frame()
                 
                 if temporal_filter:
                     depth_image = self._temporal_filter(depth_image, prev_depth_image)
@@ -104,28 +91,8 @@ class DepthCamera :
                 
             ##----------------------------------------------------------------------------------------------------
             # GET COLOR FRAME 
-            if color:
-                _, color_image = cap.read()
-                  
-                if temporal_filter:
-                    color_image = self._temporal_filter(color_image, prev_color_image)
-                    prev_color_image = color_image
-
-                ##--------------------------------------------------------------------------------------------------
-                # GET YOLO ANNOTATION 
-                
-                if yolo:
-                    annotator = Annotator(color_image)
-                    
-                    results = self.model.predict(color_image, verbose=False)
-                    if results:
-                        for r in results:
-                            if r.boxes and r.masks: 
-                                for box, mask in zip(r.boxes, r.masks):
-                                    color_image, annotator = self._annotate_segment(color_image, box, mask, annotator, img_depth)
-                                        
-                    color_image = annotator.result()         
-                
+            if color:       
+                color_image = color_stream.get_frame(img_depth=img_depth)
                 cv2.imshow("Color Image", color_image,)
                             
             ##----------------------------------------------------------------------------------------------------
@@ -135,12 +102,11 @@ class DepthCamera :
                 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-            
+    
             i+=1
         
-        openni2.unload()
-        cap.release()
-        cv2.destroyAllWindows()
+        depth_stream.close()
+        color_stream.close()
         
     ##-----------------------------------------------------------------------------------------------
     ## Add Extra Functions
@@ -149,7 +115,89 @@ class DepthCamera :
         model = YOLO(YOLO_SEG_MODEL_PATH)
         return model.to(self.device)
     
-    def _annotate_segment(self, img, box, mask, annotator, img_depth=None):
+    def _temporal_filter(self, frame, prev_frame=None, alpha=0.5):
+        if prev_frame is None : 
+            return frame
+        else : 
+            result = cv2.addWeighted(frame, alpha, prev_frame, 1-alpha, 0)
+            return result
+
+
+class DepthStream :
+    def __init__(self, redist, width, height, fps, temporal_filter=False):
+        openni2.initialize(redist)
+        dev = openni2.Device.open_any()
+        self.depth_stream = dev.create_depth_stream()
+        self.depth_stream.start()
+        self.depth_stream.set_video_mode(c_api.OniVideoMode(pixelFormat = c_api.OniPixelFormat.ONI_PIXEL_FORMAT_DEPTH_1_MM, resolutionX = width, resolutionY = height, fps = fps))
+        
+        self.temporal_filter = temporal_filter
+        if self.temporal_filter:
+            self.prev_depth_image = None
+    
+    def get_frame(self, colormap=False):
+        depth_frame = self.depth_stream.read_frame()
+        depth_frame_data = depth_frame.get_buffer_as_uint16()
+        img_depth = np.frombuffer(depth_frame_data, dtype=np.uint16).astype(np.float32)
+        img_depth.shape = (480, 640)
+        depth_image = cv2.normalize(img_depth, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        if colormap:
+            depth_image = cv2.applyColorMap(depth_image, cv2.COLORMAP_JET)
+        
+        if self.temporal_filter :
+            depth_image = self._temporal_filter(depth_image, self.prev_depth_image)
+            self.prev_depth_image = depth_image
+        
+        return depth_image, img_depth
+    
+    def _temporal_filter(self, frame, prev_frame=None, alpha=0.5):
+        if prev_frame is None : 
+            return frame
+        else : 
+            result = cv2.addWeighted(frame, alpha, prev_frame, 1-alpha, 0)
+            return result
+    
+    def close(self):
+        openni2.unload()
+
+class ColorStream:
+    def __init__(self, cam, model=None, temporal_filter=False):
+        self.cap = cv2.VideoCapture(cam)
+        self.model = model
+        
+        self.temporal_filter = temporal_filter
+        if self.temporal_filter:
+            self.prev_color_image = None
+            
+    def get_frame(self, img_depth=None):
+        _, color_image = self.cap.read()
+        
+        if self.model is not None:
+            color_image = self._yolo(color_image, img_depth)
+        
+        if self.temporal_filter :
+            color_image = self._temporal_filter(color_image, self.prev_color_image)
+            self.prev_color_image = color_image
+        
+        return color_image
+    
+    def close(self):
+        self.cap.release()
+        cv2.destroyAllWindows()
+    
+    def _yolo(self, img, img_depth=None):
+        annotator = Annotator(img)
+        results = self.model.predict(img, verbose=False)
+        if results:
+            for r in results:
+                if r.boxes and r.masks: 
+                    for box, mask in zip(r.boxes, r.masks):
+                        img, annotator = self._annotate_segment(img, box, mask, annotator, img_depth)
+                            
+        img = annotator.result() 
+        return img
+    
+    def _annotate_segment(self, img, box, mask, annotator, img_depth) : 
         bbox = box.xyxy[0]
         class_name = box.cls
         border = mask.xy[0]
@@ -161,14 +209,14 @@ class DepthCamera :
             img = self._add_distance_estimation(img, mask, img_depth, bbox)
         
         return img, annotator
-    
+
     def _add_border(self, img, border):
         for a, b in border:
             img[int(b)-1, int(a)-1, 0] = 0
             img[int(b)-1, int(a)-1, 1] = 0
             img[int(b)-1, int(a)-1, 2] = 255
         return img
-    
+
     def _add_distance_estimation(self, img, mask, img_depth, bbox):
         mask_segment = mask.data.to(self.device).numpy()
         mask_segment.shape = (480, 640)
@@ -180,7 +228,7 @@ class DepthCamera :
         img = cv2.putText(img, f'{depth_estimation} cm', center, DEFAULT_FONT,  
                 0.4, (0, 0, 255), 1, DEFAULT_LINE)
         return img
-        
+    
     def _temporal_filter(self, frame, prev_frame=None, alpha=0.5):
         if prev_frame is None : 
             return frame
@@ -188,6 +236,7 @@ class DepthCamera :
             result = cv2.addWeighted(frame, alpha, prev_frame, 1-alpha, 0)
             return result
     
+
 class CameraData :
     def __init__(self, data_dir, color=True, depth=True, depth_data=True):
         self.color = color
